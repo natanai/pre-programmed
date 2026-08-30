650b28ecb1dd7ed4496fd9824da97ae69375f217
natanai <109430106+natanai@users.noreply.github.com>
2026-08-30 09:15:07 -0500

    Split GitHub Pages client from Cloudflare API

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { cloudflare } from "@cloudflare/vite-plugin";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-export default defineConfig({
-  plugins: [react(), cloudflare()],
-});
+export default defineConfig(({ mode }) => ({
+  base: mode === "pages" ? "/pre-programmed/" : "/",
+  plugins: [react()],
+}));
