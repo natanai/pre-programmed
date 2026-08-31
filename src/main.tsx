0481091d7d98c1774f71bdc9b65c7543f94decf1
natanai <109430106+natanai@users.noreply.github.com>
2026-08-31 13:31:06 -0500

    Remove global terminal caret bootstrap

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./styles.css";
 import "./ui/terminalPrompt.css";
-import "./ui/terminalPrompt";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
