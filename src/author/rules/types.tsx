97231c368fc2dfd210b1229b998bd4a447b21a5b
natanai <109430106+natanai@users.noreply.github.com>
2026-08-30 23:03:54 -0500

    Define Author rule adapter contracts

---
+++ src/author/rules/types.tsx
@@ -0,0 +1,35 @@
+import type { ReactNode } from "react";
+import type { Condition, Effect, ProjectSnapshot } from "../../game/model";
+
+export type RenderNestedCondition = (
+  condition: Condition,
+  onChange: (condition: Condition) => void,
+) => ReactNode;
+
+export type ConditionAuthorContext = {
+  condition: Condition;
+  snapshot: ProjectSnapshot;
+  onChange: (condition: Condition) => void;
+  depth: number;
+  renderNested: RenderNestedCondition;
+};
+
+export type ConditionAuthorAdapter = {
+  type: Condition["type"];
+  label: string;
+  create: () => Condition;
+  render: (context: ConditionAuthorContext) => ReactNode;
+};
+
+export type EffectAuthorContext = {
+  effect: Effect;
+  snapshot: ProjectSnapshot;
+  onChange: (effect: Effect) => void;
+};
+
+export type EffectAuthorAdapter = {
+  type: Effect["type"];
+  label: string;
+  create: () => Effect;
+  render: (context: EffectAuthorContext) => ReactNode;
+};
