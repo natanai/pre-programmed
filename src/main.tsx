704cd0c8f0d574f0aed1b25c41c5255f759d1066
natanai <109430106+natanai@users.noreply.github.com>
2026-08-31 10:10:45 -0500

    Load terminal prompt presentation

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./styles.css";
+import "./ui/terminalPrompt.css";
+import "./ui/terminalPrompt";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
