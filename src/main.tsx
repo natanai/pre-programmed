860cf3a98875f7366c712adabd67ef4fa665cd8c
natanai <109430106+natanai@users.noreply.github.com>
2026-08-31 14:27:03 -0500

    Install canonical Author workspace shell

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./styles.css";
 import "./ui/terminalPrompt.css";
+import "./author/authorWorkspaceShell.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
