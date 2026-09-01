325b5feeac14ba37e9e9b1f50d8074840d264192
natanai <109430106+natanai@users.noreply.github.com>
2026-09-01 13:57:06 -0500

    Keep rule adapters independent of task plumbing

--- src/author/rules/types.tsx
+++ src/author/rules/types.tsx
@@ -1,6 +1,5 @@
 import type { ReactNode } from "react";
 import type { Condition, Effect, ProjectSnapshot } from "../../game/model";
-import type { AuthorResourceTools } from "../resources/types";
 
 export type RenderNestedCondition = (
   condition: Condition,
@@ -10,7 +9,6 @@ export type RenderNestedCondition = (
 export type ConditionAuthorContext = {
   condition: Condition;
   snapshot: ProjectSnapshot;
-  resources: AuthorResourceTools;
   onChange: (condition: Condition) => void;
   depth: number;
   renderNested: RenderNestedCondition;
@@ -26,7 +24,6 @@ export type ConditionAuthorAdapter = {
 export type EffectAuthorContext = {
   effect: Effect;
   snapshot: ProjectSnapshot;
-  resources: AuthorResourceTools;
   onChange: (effect: Effect) => void;
 };
 
