2468cb2da0a70d2f2ec410c174b3064048be5469
natanai <109430106+natanai@users.noreply.github.com>
2026-09-02 16:39:04 -0500

    Load equipment placement styles with Inventory feature

--- src/features/inventory/author/manifest.tsx
+++ src/features/inventory/author/manifest.tsx
@@ -14,6 +14,7 @@ import {
   setItemStateEffectAdapter,
 } from "./ruleAdapters";
 import { inventoryProjectReferences } from "./references";
+import "./equipmentPlacement.css";
 
 export const inventoryAuthorFeature: AuthorFeatureManifest = {
   id: "inventory",
