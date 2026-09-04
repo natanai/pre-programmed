ab9721809351abc5731b4ab92f712f0729aec098
natanai <109430106+natanai@users.noreply.github.com>
2026-09-03 21:05:05 -0500

    Load author locations layout styles

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import "./styles.css";
 import "./ui/terminalPrompt.css";
 import "./author/authorWorkspaceShell.css";
 import "./author/authorDesktopSuite.css";
+import "./author/workspace/workspacePanelLocations.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
