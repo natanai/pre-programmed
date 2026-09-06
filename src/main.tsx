f9b35222f8caa3bbd12b2fe45dacf4bd98745f20
natanai <109430106+natanai@users.noreply.github.com>
2026-09-06 12:41:20 -0500

    Preload reachable inventory art during launch

--- src/main.tsx
+++ src/main.tsx
@@ -9,12 +9,20 @@ import "./author/authorDesktopSuite.css";
 import { installAuthorNumberInputScrubbing } from "./author/ui/authorNumberInputScrubbing";
 import { primeProjectSnapshot } from "./data/api";
 import { saveCachedSnapshot } from "./data/localProject";
+import { isPlaySessionCompatible, loadPlaySession } from "./data/localPlaySession";
+import { immediatelyReachableInventoryAssetIds } from "./features/inventory/playerAssets";
+import { preloadImageAssets } from "./platform/assets/preload";
 import { installationSettings } from "./platform/installation/settings";
 import { UniverseBootstrap } from "./ui/UniverseBootstrap";
 
 installAuthorNumberInputScrubbing();
 
+const playerSessionReady = loadPlaySession();
 const projectReady = primeProjectSnapshot().then(async (project) => {
+  void playerSessionReady.then((session) => {
+    const state = session && isPlaySessionCompatible(project, session) ? session.playState : null;
+    return preloadImageAssets(project, immediatelyReachableInventoryAssetIds(project, state));
+  });
   await saveCachedSnapshot(project);
   return project;
 });
