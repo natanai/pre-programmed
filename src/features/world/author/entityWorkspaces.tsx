4f530ca39441459cbd483f211257172ccfbd48d0
natanai <109430106+natanai@users.noreply.github.com>
2026-09-07 12:09:03 -0500

    author: route world deletion through shared resource completion

--- src/features/world/author/entityWorkspaces.tsx
+++ src/features/world/author/entityWorkspaces.tsx
@@ -1,7 +1,6 @@
 import { resolveAuthorKey } from "../../../author/generatedKey";
 import { OperationHooksEditor } from "../../../author/operations/OperationHooksEditor";
 import { referencesTo } from "../../../author/references/projectReferences";
-import { completeAuthorResourceDeletion } from "../../../author/resources/completion";
 import { defineAuthorWorkspace } from "../../../author/ui/workspaceDefinition";
 import type { EntityDefinition } from "../model";
 import "./worldWorkspaces.css";
@@ -75,7 +74,7 @@ export const worldEntityWorkspace = defineAuthorWorkspace<EntityDefinition>({
         `Delete ${draft.type} ${label}`,
       );
       if (result.status !== "saved" && result.status !== "queued") return;
-      completeAuthorResourceDeletion(context, route.data?.resourceTask ?? draft.type, draft.id);
+      context.leaveCurrentTask();
     };
 
     return {
